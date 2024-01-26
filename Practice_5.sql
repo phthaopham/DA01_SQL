fbda6dc0d6d81cbc829968564307d8e3728687b9
phthaopham <156926130+phthaopham@users.noreply.github.com>
2024-01-26 16:42:45 +0700

    Practice_5.sql

--- Practice_5.sql
+++ Practice_5.sql
@@ -1,5 +1,12 @@
 
-
+-- Ex 5
+SELECT b.employee_id, b.name, COUNT(a.employee_id) AS reports_count, ROUND(AVG(a.age)) AS average_age
+FROM employees AS a
+JOIN employees AS b 
+ON a.reports_to = b.employee_id
+GROUP BY b.employee_id, b.name
+HAVING COUNT(a.employee_id) > 0
+ORDER BY b.employee_id;
 -- Ex 6
 SELECT product_name, SUM(unit) AS unit
 FROM products AS a
@@ -9,9 +16,10 @@ WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY product_name
 HAVING SUM(unit) >= 100;
 -- Ex 7
-SELECT pages.page_id
-FROM pages
-LEFT JOIN page_likes
-ON pages.page_id = page_likes.page_id
-WHERE page_likes.user_id IS NULL
-ORDER BY pages.page_id
+SELECT a.page_id
+FROM pages AS a
+LEFT JOIN page_likes AS b
+ON a.page_id = b.page_id
+WHERE b.user_id IS NULL
+ORDER BY a.page_id ASC;
+
